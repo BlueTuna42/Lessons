3a9cf1e406cfb5a2fcc39bea56a003c5cddf57f2
BlueTuna <99135280+BlueTuna42@users.noreply.github.com>
2025-03-07 15:05:10 +0300

    Add files via upload

--- lesson5/5-1.py
+++ lesson5/5-1.py
@@ -5,34 +5,13 @@ def tobin(dec):
     return list(map(int, "{0:08b}".format(dec)))
 
 def adc():
-    a = "10000000"
-    gpio.output(dac, a)
+    for i in range(256):
+        gpio.output(dac, tobin(i))
+        time.sleep(0.001)
+        if (gpio.input(comp)):
+            return i
+    return -1
 
-    if gpio.input(comp):
-        a[7] = 0
-    a[6] = 1
-    if gpio.input(comp):
-        a[6] = 0
-    a[5] = 1
-    if gpio.input(comp):
-        a[5] = 0
-    a[4] = 1
-    if gpio.input(comp):
-        a[4] = 0
-    a[3] = 1
-    if gpio.input(comp):
-        a[3] = 0
-    a[2] = 1
-    if gpio.input(comp):
-        a[2] = 0
-    a[1] = 1
-    if gpio.input(comp):
-        a[1] = 0
-    a[0] = 1
-    if gpio.input(comp):
-        a[0] = 0
-
-    return a
 
 gpio.setmode(gpio.BCM)
 
@@ -46,7 +25,11 @@ gpio.setup(comp, gpio.IN)
 
 try:
     while True:
-        val = tobin(adc())
+        val = adc()
+        if val < 0:
+            print(">3.3V")
+            continue
+
         print(str(val/256 * 3.3) + " V")
 finally:
     gpio.output(dac, 0)
